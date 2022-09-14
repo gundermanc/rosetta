b60f0c162b3f9b320e831c26c39175e8040b4212
Christian Gunderman <chgund@microsoft.com>
2022-09-13 20:39:02 -0700

    Text synchronization in language server.

--- src/Rosetta.Server/LanguageServer.cs
+++ src/Rosetta.Server/LanguageServer.cs
@@ -5,10 +5,16 @@
     using System;
     using StreamJsonRpc;
     using Microsoft.VisualStudio.LanguageServer.Protocol;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.Text;
+    using System.Linq;
+    using System.Diagnostics;
 
     internal sealed class LanguageServer
     {
+        private readonly MefHostCompositionService mefHost = new();
         private readonly JsonRpc rpc;
+        private readonly Dictionary<Uri, ITextBuffer> openDocuments = new();
 
         public static async Task RunServerAsync(Stream input, Stream output)
         {
@@ -45,7 +51,7 @@
                     TextDocumentSync = new TextDocumentSyncOptions()
                     {
                         OpenClose = true,
-                        Change = TextDocumentSyncKind.Full,
+                        Change = TextDocumentSyncKind.Incremental,
                     },
                 },
             };
@@ -54,19 +60,75 @@
         [JsonRpcMethod(Methods.TextDocumentDidOpenName, UseSingleObjectParameterDeserialization = true)]
         public void OnTextDocumentOpened(DidOpenTextDocumentParams arg)
         {
-            throw new NotImplementedException();
+            lock (this.openDocuments)
+            {
+                if (this.openDocuments.ContainsKey(arg.TextDocument.Uri))
+                {
+                    throw new InvalidOperationException("Document is already open");
+                }
+
+                this.openDocuments.Add(arg.TextDocument.Uri, this.BufferFactoryService.CreateTextBuffer());
+            }
         }
 
         [JsonRpcMethod(Methods.TextDocumentDidCloseName, UseSingleObjectParameterDeserialization = true)]
         public void OnTextDocumentClosed(DidCloseTextDocumentParams arg)
         {
-            throw new NotImplementedException();
+            lock (this.openDocuments)
+            {
+                if (!this.openDocuments.Remove(arg.TextDocument.Uri))
+                {
+                    throw new InvalidOperationException("Document is not yet open");
+                }
+            }
         }
 
         [JsonRpcMethod(Methods.TextDocumentDidChangeName, UseSingleObjectParameterDeserialization = true)]
         public void OnTextDocumentChanged(DidChangeTextDocumentParams arg)
         {
-            throw new NotImplementedException();
+            ITextSnapshot newSnapshot;
+
+            lock (this.openDocuments)
+            {
+                if (!this.openDocuments.TryGetValue(arg.TextDocument.Uri, out var buffer))
+                {
+                    throw new InvalidOperationException("Document is not yet open");
+                }
+
+                using (var edit = buffer.CreateEdit())
+                {
+                    foreach (var change in arg
+                        .ContentChanges
+                        .OrderByDescending(change => change.Range.Start.Line))
+                    {
+                        var start = edit.Snapshot.GetLineFromLineNumber(change.Range.Start.Line).Start + change.Range.Start.Character;
+                        var end = edit.Snapshot.GetLineFromLineNumber(change.Range.End.Line).Start + change.Range.End.Character;
+
+                        edit.Replace(new SnapshotSpan(start, end), change.Text);
+                    }
+
+                    newSnapshot = edit.Apply();
+                }
+            }
+
+            DebugPrintDocument(newSnapshot);
+        }
+
+        private ITextBufferFactoryService BufferFactoryService
+            => this.mefHost.GetExportedValue<ITextBufferFactoryService>() ??
+            throw new InvalidOperationException("Unable to acquire text buffer factory");
+
+        [Conditional("DEBUG")]
+        private void DebugPrintDocument(ITextSnapshot snapshot)
+        {
+            Console.WriteLine($"**************************** v{snapshot.Version.VersionNumber}****************************");
+
+            foreach (var line in snapshot.Lines)
+            {
+                Console.WriteLine(line.GetText());
+            }
+
+            Console.WriteLine($"******************************************************************************************");
         }
     }
 }
---
+++ src/Rosetta.Server/MefHostCompositionService.cs
@@ -0,0 +1,135 @@
+﻿namespace Rosetta.Server
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.ComponentModel.Composition.Hosting;
+    using System.ComponentModel.Composition.Primitives;
+    using System.Reflection;
+    using Microsoft.VisualStudio.Text;
+    using Microsoft.VisualStudio.Text.Editor;
+    using Microsoft.VisualStudio.Threading;
+    using Microsoft.VisualStudio.Utilities;
+
+    internal sealed partial class MefHostCompositionService : IDisposable
+    {
+        private readonly CompositionContainer compositionContainer;
+        private readonly CompositionBatch compositionBatch;
+
+        private bool isComposed;
+        private bool isDisposed;
+
+        [Export]
+        private JoinableTaskContext joinableTaskContext = new JoinableTaskContext();
+
+        public MefHostCompositionService(bool composeImmediately = true)
+        {
+            Assembly serviceAssembly = Assembly.GetExecutingAssembly();
+
+            AggregateCatalog compositionCatalog = new AggregateCatalog();
+            compositionCatalog.Catalogs.Add(new AssemblyCatalog(typeof(IContentType).Assembly));    // Microsoft.VisualStudio.CoreUtility.dll
+            compositionCatalog.Catalogs.Add(new AssemblyCatalog(typeof(ITextBuffer).Assembly));     // Microsoft.VisualStudio.Text.Data.dll
+            compositionCatalog.Catalogs.Add(new AssemblyCatalog(typeof(IEditorOptions).Assembly));  // Microsoft.VisualStudio.Text.Logic.dll
+            compositionCatalog.Catalogs.Add(new AssemblyCatalog(typeof(Microsoft.VisualStudio.Text.Utilities.IEncodingDetectorMetadata).Assembly));      // Microsoft.VisualStudio.Text.Core.Implementation.dll
+            compositionCatalog.Catalogs.Add(new AssemblyCatalog(serviceAssembly));
+
+            // The composition is going to be accessed from multiple threads so require it to be thread safe, see https://devdiv.visualstudio.com/DevDiv/_workitems/edit/1231274
+            this.compositionContainer = new CompositionContainer(compositionCatalog, isThreadSafe: true);
+            this.compositionBatch = new CompositionBatch();
+            this.compositionBatch.AddPart(this);
+
+            if (composeImmediately)
+            {
+                this.Compose();
+            }
+        }
+
+        public void Compose()
+        {
+            this.ThrowIfDisposed();
+
+            if (!this.isComposed)
+            {
+                this.compositionContainer.Compose(this.compositionBatch);
+                this.isComposed = true;
+            }
+        }
+
+        #region ICompositionService interface
+        public void SatisfyImportsOnce(ComposablePart part)
+        {
+            this.compositionContainer.SatisfyImportsOnce(part);
+        }
+        #endregion
+
+        #region IMefHostExportProvider interface
+        public TExport? GetExportedValue<TExport>()
+        {
+            this.ThrowIfNotComposed();
+            return this.compositionContainer.GetExportedValueOrDefault<TExport>();
+        }
+
+        public Lazy<TExport>? GetExport<TExport>()
+        {
+            this.ThrowIfNotComposed();
+            return this.compositionContainer.GetExport<TExport>();
+        }
+
+        public Lazy<TExport, TMetadata>? GetExport<TExport, TMetadata>()
+        {
+            this.ThrowIfNotComposed();
+            return this.compositionContainer.GetExport<TExport, TMetadata>();
+        }
+
+        public IEnumerable<TExport> GetExportedValues<TExport>()
+        {
+            this.ThrowIfNotComposed();
+            return this.compositionContainer.GetExportedValues<TExport>();
+        }
+
+        public IEnumerable<Lazy<TExport>> GetExports<TExport>()
+        {
+            this.ThrowIfNotComposed();
+            return this.compositionContainer.GetExports<TExport>();
+        }
+
+        public IEnumerable<Lazy<TExport, TMetadata>> GetExports<TExport, TMetadata>()
+        {
+            this.ThrowIfNotComposed();
+            return this.compositionContainer.GetExports<TExport, TMetadata>();
+        }
+        #endregion
+
+        #region IDispose
+        public void Dispose()
+        {
+            if (!this.isDisposed)
+            {
+                this.compositionContainer.Dispose();
+                this.isDisposed = true;
+
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (this.isDisposed)
+            {
+                throw new ObjectDisposedException(this.GetType().Name);
+            }
+        }
+
+        private void ThrowIfNotComposed()
+        {
+            this.ThrowIfDisposed();
+
+            if (!this.isComposed)
+            {
+                throw new InvalidOperationException("Must compose all MEF component parts before getting exports");
+            }
+        }
+
+        #endregion
+    }
+}

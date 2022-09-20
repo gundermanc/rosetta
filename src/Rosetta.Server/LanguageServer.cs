2d290aadd620d20b7c563cecb951fea8ccc78630
Christian Gunderman <chgund@microsoft.com>
2022-09-19 22:06:26 -0700

    Semantic colorization.

--- src/Rosetta.Server/LanguageServer.cs
+++ src/Rosetta.Server/LanguageServer.cs
@@ -9,6 +9,10 @@
     using Microsoft.VisualStudio.Text;
     using System.Linq;
     using System.Diagnostics;
+    using Rosetta.Analysis.Grammar;
+    using Rosetta.Analysis.Text;
+    using Rosetta.Analysis.GrammarExecution;
+    using Microsoft.VisualStudio.Utilities;
 
     internal sealed class LanguageServer
     {
@@ -53,6 +57,15 @@
                         OpenClose = true,
                         Change = TextDocumentSyncKind.Incremental,
                     },
+                    SemanticTokensOptions = new SemanticTokensOptions()
+                    {
+                        Range = true,
+                        Legend = new SemanticTokensLegend
+                        {
+                            TokenTypes = SemanticTokenTypes.AllTypes.ToArray(),
+                            TokenModifiers = SemanticTokenModifiers.AllModifiers.ToArray()
+                        },
+                    }
                 },
             };
         }
@@ -67,7 +80,13 @@
                     throw new InvalidOperationException("Document is already open");
                 }
 
-                this.openDocuments.Add(arg.TextDocument.Uri, this.BufferFactoryService.CreateTextBuffer());
+                var buffer = this.BufferFactoryService.CreateTextBuffer();
+
+                buffer.Insert(0, arg.TextDocument.Text);
+
+                this.openDocuments.Add(
+                    arg.TextDocument.Uri,
+                    buffer);
             }
         }
 
@@ -112,6 +131,98 @@
             }
 
             DebugPrintDocument(newSnapshot);
+        }
+
+        [JsonRpcMethod(Methods.TextDocumentSemanticTokensRangeName, UseSingleObjectParameterDeserialization = true)]
+        public async Task<SemanticTokens> SemanticTokensRangeAsync(SemanticTokensRangeParams arg)
+        {
+            var grammar = await GrammarParser.ParseGrammarAsync(@"C:\Repos\rosetta\src\Rosetta.Analysis.Tests\Grammars\Arithmetic.rosetta.md");
+
+            TextSnapshot? snapshot;
+
+            lock (this.openDocuments)
+            {
+                if (!this.openDocuments.TryGetValue(arg.TextDocument.Uri, out var buffer))
+                {
+                    return new SemanticTokens() { Data = Array.Empty<int>() };
+                }
+
+                snapshot = new TextSnapshot(buffer.CurrentSnapshot);
+            }
+
+            var parse = GrammarExecution.Parse(grammar, snapshot);
+
+            var builder = ArrayBuilder<int>.GetInstance();
+
+            var previousText = new SnapshotSegment(snapshot, 0, 0);
+
+            this.VisitSyntaxNode(parse.Root, builder, snapshot, ref previousText);
+
+            return new SemanticTokens()
+            {
+                Data = builder.ToArrayAndFree(),
+            };
+        }
+
+        private void VisitSyntaxNode(
+            SyntaxNode syntaxNode,
+            ArrayBuilder<int> responseBuilder,
+            TextSnapshot snapshot,
+            ref SnapshotSegment previousText)
+        {
+            var type = TypeFromRuleName(syntaxNode.RuleName);
+
+            if (type != -1)
+            {
+                var thisNodeStartLine = snapshot.Snapshot.GetLineFromPosition(syntaxNode.Text.Start);
+                var previousNodeStartLine = snapshot.Snapshot.GetLineFromPosition(previousText.Start);
+
+                // Lines delta from last response.
+                responseBuilder.Add(thisNodeStartLine.LineNumber - previousNodeStartLine.LineNumber);
+
+                // Horizontal offset delta from previous response on this line, if any.
+                responseBuilder.Add(
+                    thisNodeStartLine.LineNumber == previousNodeStartLine.LineNumber ?
+                    syntaxNode.Text.Start - previousText.Start :
+                    syntaxNode.Text.Start - thisNodeStartLine.Start);
+
+                // Length of token.
+                responseBuilder.Add(syntaxNode.Text.Length);
+
+                // Token type.
+                responseBuilder.Add(type); // Keyword
+
+                // Token modifier.
+                responseBuilder.Add(0);
+
+                previousText = syntaxNode.Text;
+            }
+
+            foreach (var node in syntaxNode.Children)
+            {
+                VisitSyntaxNode(
+                    node,
+                    responseBuilder,
+                    snapshot,
+                    ref previousText);
+            }
+        }
+
+        private int TypeFromRuleName(string ruleName)
+        {
+            // TODO: these can be cached and done without a list copy.
+            if (ruleName.Contains("NUMBER"))
+            {
+                return SemanticTokenTypes.AllTypes.ToList().IndexOf(SemanticTokenTypes.String);
+            }
+            else if (ruleName.Contains("OPERATOR"))
+            {
+                return SemanticTokenTypes.AllTypes.ToList().IndexOf(SemanticTokenTypes.Keyword);
+            }
+            else
+            {
+                return -1;
+            }
         }
 
         private ITextBufferFactoryService BufferFactoryService
---
+++ src/Rosetta.Server/TextSnapshot.cs
@@ -0,0 +1,19 @@
+﻿namespace Rosetta.Server
+{
+    using System;
+    using Microsoft.VisualStudio.Text;
+    using Rosetta.Analysis.Text;
+
+    internal sealed class TextSnapshot : SnapshotBase
+    {
+        public TextSnapshot(ITextSnapshot snapshot) : base(snapshot.Length)
+        {
+            this.Snapshot = snapshot
+                ?? throw new ArgumentNullException(nameof(snapshot));
+        }
+
+        public override char this[int offset] => this.Snapshot[offset];
+
+        public ITextSnapshot Snapshot { get; }
+    }
+}
